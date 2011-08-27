214b64c3c12381cab60ebcee5be5645e7130519d
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2011-08-27 07:24:54 -0300

    Started work on nowjs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require.paths.unshift(__dirname+'/lib');
 
 require('server')(
-    require('express')
-  , require('everyauth')
-
+  require('express'),
+  require('everyauth'),
+  require('now'),
   // Controllers
-  ,[
-    require('controllers/index')
-  , require('controllers/conference')
-  , require('controllers/tweetalk')
+  [
+    require('controllers/index'),
+    require('controllers/conference'),
+    require('controllers/tweetalk')
   ]
 );
---
+++ public/javascripts/tweetalk.js
@@ -0,0 +1,8 @@
+$(function() {
+  now.ready(function() {
+
+  });
+  now.renderTweet = function(tweet) {
+    console.log(tweet);
+  };
+});
